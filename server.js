b76f782b69c570ae0f9ec775382e10a50ad6cce0
Darmin <254663604@qq.com>
2020-12-08 20:15:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Hello World!  Hello!');
+    response.end('Hello World! Hello!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
