306840cb4112737c8213f92e5d98c3484ca531f8
salomezhang <salomezhang@tencent.com>
2021-04-08 17:20:43 +0800

    test:增加main push通知

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var handleRequest = function(request, response) {
-    console.log('console.log push');
+    console.log('console.log push2');
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
     response.end('Hello World! ');
