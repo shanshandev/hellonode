91a203b04da172dd071212baf934283fa6837e30
salomezhang <salomezhang@tencent.com>
2021-04-28 10:43:33 +0800

    feat: server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var handleRequest = function(request, response) {
     console.log('console.log push3');
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Hello World!');
+    response.end('Hello World! ');
 };
 var www = http.createServer(handleRequest);
 www.listen(80);
