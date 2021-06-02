053f2a807f14bc83841ecac4cadcf66463a931c4
salomezhang <salomezhang@tencent.com>
2021-06-02 20:08:07 +0800

    feat: test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var handleRequest = function(request, response) {
-    console.log('console.log push3');
+    console.log('console.log push3 ');
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
     response.end('Hello World! ');
